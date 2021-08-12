6ebab022abdb2f435ec680d679cb085b81f4bc09
oumar <oumardialo98@qq.com>
2021-08-12 14:33:40 +0800

    Initial[ commitdsfgs

---
+++ src/main/java/com/alpha/spring/controller/User.java
@@ -0,0 +1,15 @@
+/*
+  Created by:oumar
+  Project:spring
+  Name:User
+  Date: 8/12/2021
+  Time: 2:33 PM
+*/
+package com.alpha.spring.controller;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("euihptuieuiaspguier");
+    }
+
+}
